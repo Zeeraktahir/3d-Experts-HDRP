d2fcd714af56146e43dd123564a4dc4e37b7141d
DESKTOP-0KRAVIR\Mubeen Dar <mobeen.dar@invozone.com>
2022-10-05 13:09:11 +0500

    First Commit

---
+++ Assets/Camera Controller and PP/Scripts/InteractionUI.cs
@@ -0,0 +1,54 @@
+﻿#pragma warning disable 0649
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionUI : MonoBehaviour
+{
+    [Space]
+    [SerializeField] Toggle toggleFreePivot;
+    [SerializeField] CameraControl freePivotController;
+
+    [System.NonSerialized] bool initialized; // Not serialized to detect domain reload as UI event references are not serialized
+    void OnEnable()
+    {
+        if (!initialized)
+            Initialize();
+    }
+
+    void Start()
+    {
+        SetDefaults();
+    }
+
+    void Initialize()
+    {
+        toggleFreePivot.onValueChanged.AddListener(HandleFreePivotModeToggle);
+        initialized = true;
+    }
+
+    void SetDefaults()
+    {
+        ForceToggleValue(toggleFreePivot, true);
+    }
+
+    void ForceToggleValue(Toggle toggle, bool shouldBeOn)
+    {
+        if (toggle.isOn == shouldBeOn)
+        {
+            toggle.onValueChanged.Invoke(shouldBeOn);
+        }
+        else
+        {
+            toggle.isOn = shouldBeOn;
+        }
+    }
+
+    void HandleFreePivotModeToggle(bool enable)
+    {
+        if (enable)
+        {
+            freePivotController.ControlEnable();
+        }
+    }
+}
---
+++ Assets/OBJImport/Samples/ObjFromStream.cs
@@ -0,0 +1,115 @@
+﻿using Dummiesman;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ObjFromStream : MonoBehaviour
+{
+    public Shader masterShader;
+
+    //[SerializeField]
+    //Texture texture;
+
+    public static ObjFromStream instance;
+    public List<GameObject> loadedObj;
+    public GameObject ParentObject;
+    string file_path = "E:/Invozone/Invozone Projects/3D experts streaming project/Blender Projects/Ulco_Model_3_0.3_decimation - Temp/";
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(GetRequest());
+    }
+
+    void LoadObjsFromDirectory()
+    {
+        //string file_path = "E:/Invozone/Invozone Projects/3D experts streaming project/Meshes/Heater";
+        var info = new DirectoryInfo(file_path);
+        var fileInfoObjs = info.GetFiles("*.obj");
+        var fileInfoMtls = info.GetFiles("*.mtl");
+        int i = 0;
+        foreach (FileInfo file in fileInfoObjs)
+        {
+
+            var wwwObj = new WWW(file.ToString());
+            while (!wwwObj.isDone)
+                System.Threading.Thread.Sleep(1);
+
+            var wwwMtl = new WWW(fileInfoMtls[i].ToString());
+            while (!wwwMtl.isDone)
+                System.Threading.Thread.Sleep(1);
+
+            //create stream and load
+            var textStreamObj = new MemoryStream(Encoding.UTF8.GetBytes(wwwObj.text));
+            var textStreamMtl = new MemoryStream(Encoding.UTF8.GetBytes(wwwMtl.text));
+            loadedObj.Add(new OBJLoader().Load(textStreamObj, textStreamMtl));
+            loadedObj[i].transform.parent = ParentObject.transform;
+            i++;
+        }
+    }
+
+    IEnumerator GetRequest()
+    {
+        var info = new DirectoryInfo(file_path);
+        var fileInfoObjs = info.GetFiles("*.obj");
+        var fileInfoMtls = info.GetFiles("*.mtl");
+        int i = 0;
+        foreach (FileInfo file in fileInfoObjs)
+        {
+            using (UnityWebRequest webRequest = UnityWebRequest.Get(file.ToString()))
+            {
+                // Request and wait for the desired page.
+                yield return webRequest.SendWebRequest();
+
+                string[] pages = file.ToString().Split('/');
+                int page = pages.Length - 1;
+
+                switch (webRequest.result)
+                {
+                    case UnityWebRequest.Result.ConnectionError:
+                    case UnityWebRequest.Result.DataProcessingError:
+                        Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.ProtocolError:
+                        Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                        break;
+                    case UnityWebRequest.Result.Success:
+                        Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                        //create stream and load
+                        var textStreamObj = new MemoryStream(Encoding.UTF8.GetBytes(webRequest.downloadHandler.text));
+
+                        UnityWebRequest webRequestMtl = UnityWebRequest.Get(fileInfoMtls[i].ToString());
+                        yield return webRequestMtl.SendWebRequest();
+
+                        switch (webRequestMtl.result)
+                        {
+                            case UnityWebRequest.Result.DataProcessingError:
+                                Debug.Log("Mtl not found");
+                                loadedObj.Add(new OBJLoader().Load(textStreamObj));
+                                loadedObj[i].transform.parent = ParentObject.transform;
+                                break;
+                            case UnityWebRequest.Result.Success:
+                                Debug.Log("Mtl found");
+                                var textStreamMtl = new MemoryStream(Encoding.UTF8.GetBytes(webRequestMtl.downloadHandler.text));
+                                loadedObj.Add(new OBJLoader().Load(textStreamObj, textStreamMtl));
+                                loadedObj[i].transform.parent = ParentObject.transform;
+                                break;
+                        }
+                        break;
+                }
+            }
+            i++;
+        }
+    }
+
+}
