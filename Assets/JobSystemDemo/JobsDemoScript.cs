3b6697294dad4d4575192e7ed8d10278bbc94673
Zeerak Tahir <zeerak.tahir@invozone.com>
2022-10-10 16:09:27 +0500

    Update

--- Assets/JobSystemDemo/JobsDemoScript.cs
+++ Assets/JobSystemDemo/JobsDemoScript.cs
@@ -3,27 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
 using Unity.Jobs;
+using Unity.Collections;
+using Unity.Burst;
 public class JobsDemoScript : MonoBehaviour
 {
     [SerializeField] private bool useJobsSystem;
 
+    [SerializeField] private Transform Pfplants;
 
+    private List<Zombie> plantList;
+    
+    
+    public class Zombie
+    {
+        public Transform transform;
+        public float moveY;
+    }
+
+    private void Start()
+    {
+        plantList = new List<Zombie>();
+        for (int i = 0; i < 1000; i++)
+        {
+            Transform zombieTransform = Instantiate(Pfplants, new Vector3(UnityEngine.Random.Range(-10f, 10f), UnityEngine.Random.Range(-10f, 10f)), Quaternion.identity);
+        }
+        
+    }
     private void Update()
       {
         float startTime = Time.realtimeSinceStartup;
         if (useJobsSystem)
         {
-            JobHandle newjob = JobSystemHandler();
-            newjob.Complete();
+            NativeList<JobHandle> jobs = new NativeList<JobHandle>(Allocator.Temp);
+            for (int i = 0; i < 10; i++)
+            {
+                
+                JobHandle jobHandle = TaskJob();
+                jobs.Add(jobHandle);
 
+            }
+            JobHandle.CompleteAll(jobs);
+            jobs.Dispose();
+               // newjob.Complete();
         }
         else
         {
-            ExampleJob();
-
+            for (int i = 0; i < 10; i++)
+            {
+                ExampleJob();
+            }
         }
-        
-        
         Debug.Log((Time.realtimeSinceStartup - startTime) * 1000f + "ms");
 
     }
@@ -31,24 +60,25 @@ public class JobsDemoScript : MonoBehaviour
     public void ExampleJob()
     {
         float value = 0f;
-        for (int i = 0; i < 999999; i++)
+        for (int i = 0; i < 99999; i++)
         {
             value = math.exp10(math.sqrt(value));
         }
 
     }
-    private JobHandle JobSystemHandler()
+    private JobHandle TaskJob()
     {
         JobStruct jobs = new JobStruct();
         return jobs.Schedule();
     }
+
+    [BurstCompile]
     public struct JobStruct : IJob
     {
-
         public void Execute()
         {
             float value = 0f;
-            for (int i = 0; i < 999999; i++)
+            for (int i = 0; i < 99999; i++)
             {
                 value = math.exp10(math.sqrt(value));
             }
